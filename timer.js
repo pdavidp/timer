025c14ac59a601d26c8129efbd49a6182e577662
David Picariello <git@davidpicariello.com>
2022-11-26 22:11:45 -0500

    Restyled and improved js

--- timer.js
+++ timer.js
@@ -1,53 +1,61 @@
-var countDownTime = 30;
-var rounds = 4;
-var breakLength = 5;
-var totalCountDownTime = (countDownTime + 1) * rounds; /* +1 because countDownTime includes 0 */
-var totalBreakTime = breakLength * rounds;
-var totalTimeLeft = totalCountDownTime + totalBreakTime;
-var lastSecond = true;
+const countDownTime = 30;
+const rounds = 4;
+const breakLength = 5;
+const totalCountDownTime = (countDownTime + 1) * rounds; /* +1 because countDownTime includes 0 */
+const totalBreakTime = breakLength * rounds;
+const allRight = new Audio('AllRightGoodForNow.mp3');
+const letsGo = new Audio('LetsGo.mp3');
+const youreDone = new Audio('You-reDone.mp3');
 
-var allRight = new Audio('AllRightGoodForNow.mp3');
-var letsGo = new Audio('LetsGo.mp3');
-var youreDone = new Audio('You-reDone.mp3');
+let totalTimeLeft = totalCountDownTime + totalBreakTime;
+let lastSecond = true;
 
 function countDown() {
-  for (var round = 1; round <= rounds; round++) {
-    for (var timer = 0; timer <= countDownTime; timer++) {
-      if (lastSecond) {
-        // special case: very last count down, done
-        window.setTimeout(setTimerDisplay, totalTimeLeft * 1000, "&#x2714;", "Round number: " + round, "");
-        lastSecond = false;
-      } else {
-        window.setTimeout(setTimerDisplay, totalTimeLeft * 1000, timer, "Round number: " + round, "");
-      }
-      totalTimeLeft--;
-    }
-    for (var b = breakLength; b >= 1; b--) {
-      var leftInBreak = breakLength - b + 1;
-      if (b === totalTimeLeft) {
-        /* get ready, not a break */
-        window.setTimeout(setTimerDisplay, totalTimeLeft * 1000, "&#x1f6b6;", "", "Get ready: " + leftInBreak);
-      } else {
-        /* break */
-        window.setTimeout(setTimerDisplay, totalTimeLeft * 1000, "&#x23F3;", "", "Left in break: " + leftInBreak);
-      }
-      totalTimeLeft--;
-    }
+  // Generate timeouts from end to start
+  for (let round = 1; round <= rounds; round++) {
+    generateRoundTimer(round);
+    generateRoundBreak(round);
   }
 }
 
-function setTimerDisplay(timeLeftThisRound, roundsRemaining, breakTimeRemaining) {
-  document.getElementById("timer").innerHTML = timeLeftThisRound;
-  document.getElementById("rounds").innerHTML = roundsRemaining;
-  document.getElementById("break").innerHTML = breakTimeRemaining;
+function generateRoundTimer(round) {
+  for (let timer = 0; timer <= countDownTime; timer++) {
+    if (lastSecond) {
+      // special case: very last count down, done
+      window.setTimeout(setTimerDisplay, totalTimeLeft * 1000, "&#x2714;", "Round number: " + round);
+      lastSecond = false;
+    } else {
+      window.setTimeout(setTimerDisplay, totalTimeLeft * 1000, timer, "Round number: " + round);
+    }
+    totalTimeLeft--;
+  }
+}
+
+function generateRoundBreak(round) {
+  for (let b = breakLength; b >= 1; b--) {
+    let leftInBreak = breakLength - b + 1;
+    if (b === totalTimeLeft) {
+      // get ready, not a break
+      window.setTimeout(setTimerDisplay, totalTimeLeft * 1000, "&#x1f6b6;", "Get ready: " + leftInBreak);
+    } else {
+      // break
+      window.setTimeout(setTimerDisplay, totalTimeLeft * 1000, "&#x23F3;", "Left in break: " + leftInBreak);
+    }
+    totalTimeLeft--;
+  }
+}
+
+function setTimerDisplay(timerText, message) {
+  document.getElementById("timer").innerHTML = timerText;
+  document.getElementById("message").innerHTML = message;
   document.getElementById("resetButton").style.display = "inline-block";
   document.getElementById("startButton").style.display = "none";
 
-  if (timeLeftThisRound === 0) {
+  if (timerText === 0) {
     allRight.play();
-  } else if (timeLeftThisRound === countDownTime) {
+  } else if (timerText === countDownTime) {
     letsGo.play();
-  } else if (timeLeftThisRound === "&#x2714;") {
+  } else if (timerText === "&#x2714;") {
     youreDone.play();
     document.getElementById("resetButton").display = "none";
     document.getElementById("startButton").display = "inline-block";
